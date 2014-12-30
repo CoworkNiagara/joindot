bdf621dfc36d8aebc6de338b0be76cce2802d4fb
Adam Daniels <adam@mediadrive.ca>
2014-12-29 22:46:12 -0500

    Update example config

--- config.example.php
+++ config.example.php
@@ -2,3 +2,5 @@
 
 define("STRIPE_SECRET_KEY", "THE_STRIPE_SECRET_KEY");
 define("STRIPE_PUBLIC_KEY", "THE_STRIPE_PUBLIC_KEY");
+define("MAIL_FROM", "Cowork Niagara <coworkniagara@gmail.com>");
+define("ADMIN_EMAIL", "coworkniagara@gmail.com");
