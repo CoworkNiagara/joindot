b7e1a16a838d442207b4bb3a5d8b838bc51a23b6
Adam Daniels <adam@mediadrive.ca>
2015-01-05 17:45:01 -0500

    Update Admin email template

--- views/admin_email.php
+++ views/admin_email.php
@@ -1,1 +1,59 @@
-Admin Email
+<p>
+    <strong>First Name:</strong>
+    <?= $_POST["first_name"] ?>
+</p>
+
+<p>
+    <strong>Last Name:</strong>
+    <?= $_POST["last_name"] ?>
+</p>
+
+<p>
+    <strong>Company Name:</strong>
+    <?= $_POST["company_name"] ?>
+</p>
+
+<p>
+    <strong>Address:</strong>
+    <?= $_POST["address"] ?>
+</p>
+
+<p>
+    <strong>Address 2:</strong>
+    <?= $_POST["address_2"] ?>
+</p>
+
+<p>
+    <strong>City:</strong>
+    <?= $_POST["city"] ?>
+</p>
+
+<p>
+    <strong>Postal Code:</strong>
+    <?= $_POST["postal_code"] ?>
+</p>
+
+<p>
+    <strong>Phone Number:</strong>
+    <?= $_POST["phone_number"] ?>
+</p>
+
+<p>
+    <strong>Email Address:</strong>
+    <?= $_POST["email_address"] ?>
+</p>
+
+<p>
+    <strong>Newsletter Signup?</strong>
+    <?= $_POST["newsletter_signup"] ? "Yes" : "No" ?>
+</p>
+
+<p>
+    <strong>Membership Type:</strong>
+    <?= $_POST["membership_type"] ?>
+</p>
+
+<p>
+    <strong>Signup Date:</strong>
+    <?= date("Y-m-d"); ?>
+</p>
